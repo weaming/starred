9edb453262d2f7862905ceb89e861e680d80417c
weaming <garden.yuen@gmail.com>
2021-06-25 15:23:56 +0800

    add statistics.sorted.csv

--- gen-stat.sh
+++ gen-stat.sh
@@ -2,3 +2,4 @@
 
 echo Language,Count > statistics.csv
 cat README.md | grep '^#'| sed 's/^## //g' |grep -Ev '(Content|License|# Awesome)' | grep -vE '^$' | sed -E 's/ ([0-9])/,\1/g' >> statistics.csv
+cat statistics.csv | xsv sort -RNs 2 > statistics.sorted.csv
