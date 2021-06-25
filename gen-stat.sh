5ef7134602f022192105782e75253ccee1b6449b
weaming <garden.yuen@gmail.com>
2021-06-25 15:21:49 +0800

    add statistics.csv

---
+++ gen-stat.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo Language,Count > statistics.csv
+cat README.md | grep '^#'| sed 's/^## //g' |grep -Ev '(Content|License|# Awesome)' | grep -vE '^$' | sed -E 's/ ([0-9])/,\1/g' >> statistics.csv
